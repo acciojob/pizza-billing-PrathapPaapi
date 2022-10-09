bd296eca66a230954589573e13ebc69c48a6dd96
Prathap_Paapi <myselfprathap@gmail.com>
2022-10-09 16:53:10 +0530

    tried... need to check

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,27 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int calculatedbill = 0;
+
+    private int VegPizzaBasePrice = 300;
+    private int NonVegPizzaBasePrice = 400;
+    private int ExtraCheesePrice = 80;
+    private int ExtraToppingsForVegPizza = 70;
+    private int ExtraToppingsForNonvegPizza = 120;
+    private int PaperBagPrice = 20;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+
+        if(isVeg)
+            this.price = VegPizzaBasePrice;
+        else
+            this.price = NonVegPizzaBasePrice;
+
+        this.calculatedbill += this.price;
+    }
+
+    public void Deluxe(boolean isDeluxe){
+        this.calculatedbill += 120;
     }
 
     public int getPrice(){
@@ -16,19 +34,26 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        this.calculatedbill += ExtraCheesePrice;
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(this.isVeg)
+            this.calculatedbill += this.ExtraToppingsForVegPizza;
+        else
+            this.calculatedbill += this.ExtraToppingsForNonvegPizza;
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.calculatedbill += this.PaperBagPrice;
     }
 
     public String getBill(){
         // your code goes here
+        this.bill = String.valueOf(this.calculatedbill);
         return this.bill;
     }
 }
